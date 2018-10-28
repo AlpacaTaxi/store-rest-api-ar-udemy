5ee1c4108ebf6f151f139193fa929bb6308f984d
AlpacaTaxi <31698304+AlpacaTaxi@users.noreply.github.com>
2018-10-28 13:04:42 -0400

    Now using postgreSQL

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #mysql, postgresql, it's just the source db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #mysql, postgresql, it's just the source db
 app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 app.config['PROPAGATE EXCEPTIONS'] = True
 app.secret_key = 'jose'
